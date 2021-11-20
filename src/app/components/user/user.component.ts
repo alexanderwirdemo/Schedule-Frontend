59e0c74c85d085e2bf554e74c8ab8347bd5d31ff
Alexander <alexander.wirdemo@gmail.com>
2021-11-20 14:42:02 +0100

    First backend get-call buildup

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,6 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
 
 interface User {
   username: string,
@@ -18,9 +20,29 @@ export class UserComponent implements OnInit {
     profileImage: 'profileImage.img'
   }
 
-  constructor() { }
+  constructor(
+    private _api: ApiService
+    ) { }
 
   ngOnInit(): void { 
-    
+    this.createCourseRegister();
+
+  }
+
+  createCourseRegister(){
+    console.log("Creating a course register, please wait....");
+    /*const token = "3755~0H049oLoUPpNxP85OmmXJf8MiSE5R7Fv4HvFPkt8GB3634QvaksVv3XqVM9DEF2A";
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+      'Authorization': 'Bearer '+token,
+    });*/
+    this._api.getTypeRequest("timeedit/api/course/"+137567).subscribe((res:any)=>{
+      console.dir(res);
+    }, err => {
+      console.log(err);
+    });
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { TouchSequence } from 'selenium-webdriver';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   baseUrl = 'http://localhost:3000/';
+  headers: HttpHeaders;
+  token: String;
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) { 
+    this.token = "3755~0H049oLoUPpNxP85OmmXJf8MiSE5R7Fv4HvFPkt8GB3634QvaksVv3XqVM9DEF2A";
+    /*this.headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+      'Authorization': 'Bearer '+this.token,
+    });*/
+    
+  }
+  
 
   getTypeRequest(url) {
+    /*const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
+      'Authorization': 'Bearer '+this.token,
+    });*/
     return this._http.get(`${this.baseUrl}${url}`).pipe(map(res => {
       console.dir(res);
     return res;
